c294278b7bc2891feb860913035cd014f62c5f5a
akashvharale <akash.v.harale@gmail.com>
2026-03-23 14:48:21 +0530

    fix:tcp server

--- services/tcpServer.js
+++ services/tcpServer.js
@@ -1,42 +1,3 @@
-/* Commented on 20/03/2026 
-// services/tcpServer.js
-require("dotenv").config();
-const net = require('net');
-const connectDB = require('../config/db');   // import existing db connection
-const GPSRawData = require('../models/gpsRawData');
-
-// Initialize MongoDB connection once when service starts
-connectDB();
-
-const tcpServer = net.createServer(socket => {
-  console.log('GPS device connected');
-
-  socket.on('data', async data => {
-    try {
-      const raw = JSON.parse(data.toString()); // replace with real parser
-      const gpsRaw = new GPSRawData({
-        imei: raw.imei,
-        raw_payload: raw,
-        raw_data: Buffer.from(data)
-      });
-      await gpsRaw.save();
-      console.log('Raw GPS packet stored:', gpsRaw._id);
-    } catch (err) {
-      console.error('Error parsing GPS packet:', err.message);
-    }
-  });
-});
-
-tcpServer.listen(3007, '0.0.0.0', () => {
-  console.log('TCP server listening on port 3007  ');
-});
-
-module.exports = tcpServer;
-*/
-
-// 20/03/2026
-// services/tcpServer.js
-
 require("dotenv").config();
 const net = require("net");
 const connectDB = require("../config/db");   // import existing db connection
@@ -114,4 +75,3 @@ process.on("unhandledRejection", err => {
 });
 
 module.exports = tcpServer;
-
